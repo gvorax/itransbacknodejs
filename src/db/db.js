4e3c7387beb77cfec74571d5afc31a58c9c3ce20
gvorax <70366257+gvorax@users.noreply.github.com>
2022-09-25 03:41:52 +0500

    asd

--- src/db/db.js
+++ src/db/db.js
@@ -3,10 +3,13 @@ require('dotenv').config({ path: 'MONGODB_URL' });
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(
+      'mongodb+srv://gvorax:Gvorax2002@cluster0.furor.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
 
     console.log('MongoDb connected');
   } catch (error) {
