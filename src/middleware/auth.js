9fea033eba6d2970121b1dbd3ff126b5a5fc9d1f
gvorax <70366257+gvorax@users.noreply.github.com>
2022-09-25 13:18:48 +0500

    asdf

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,21 +1,21 @@
 const jwt = require('jsonwebtoken');
 
-const authMiddleware =(req,res,next)=>{
+const jwtSecret = 'mysecretPassword2002#';
+
+const authMiddleware = (req, res, next) => {
   const token = req.header('x-auth-token');
 
-
-  if(!token){
-    return res.status(401).json({msg:'No token, authorization denied'});
+  if (!token) {
+    return res.status(401).json({ msg: 'No token, authorization denied' });
   }
 
   try {
-    const decoded = jwt.verify(token,process.env.PRIVATE_KEY);
-
+    const decoded = jwt.verify(token, jwtSecret);
     req.user = decoded.user;
     next();
   } catch (error) {
-    res.status(401).json({msg:'Token is not valid'})
+    res.status(401).json({ msg: 'Token is not valid' });
   }
-}
+};
 
-module.exports = authMiddleware; 
+module.exports = authMiddleware;
